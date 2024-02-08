9d523ca572ba4e5830b65aa597b346c303ab15dc
Marius Brt <marius.brouty@gmail.com>
2024-02-08 11:44:03 +0100

    Pile

---
+++ src/main/java/Pile.java
@@ -0,0 +1,62 @@
+import java.util.EmptyStackException;
+
+/**
+ * Cette classe modélise une pile, une structure de données basée sur le principe Last In First Out (LIFO).
+ * Les instances de cette classe sont mutables.
+ */
+public class Pile {
+
+    /**
+     * Construit une nouvelle pile vide.
+     */
+    public Pile() {
+        throw new UnsupportedOperationException("méthode non implémentée");
+    }
+
+    /**
+     * Empile un élément au sommet de la pile.
+     *
+     * @param element l'élément à empiler
+     */
+    public void empiler(Object element) {
+        throw new UnsupportedOperationException("méthode non implémentée");
+    }
+
+    /**
+     * Dépile l'élément au sommet de la pile.
+     *
+     * @return l'élément déplié
+     * @throws EmptyStackException si la pile est vide
+     */
+    public Object depiler() throws EmptyStackException {
+        throw new UnsupportedOperationException("méthode non implémentée");
+    }
+
+    /**
+     * Retourne l'élément au sommet de la pile sans le dépiler.
+     *
+     * @return l'élément au sommet de la pile
+     * @throws EmptyStackException si la pile est vide
+     */
+    public Object sommet() throws EmptyStackException {
+        throw new UnsupportedOperationException("méthode non implémentée");
+    }
+
+    /**
+     * Retourne le nombre d'éléments dans la pile.
+     *
+     * @return le nombre d'éléments dans la pile
+     */
+    public int taille() {
+        throw new UnsupportedOperationException("méthode non implémentée");
+    }
+
+    /**
+     * Vérifie si la pile est vide.
+     *
+     * @return true si la pile est vide, false sinon
+     */
+    public boolean estVide() {
+        throw new UnsupportedOperationException("méthode non implémentée");
+    }
+}
